ed8e91656a926304158a7dbc6719bf6cefce6f29
Rilr <Jorgelschn@gmail.com>
2024-09-04 14:51:37 -0400

    Added handling for multiple boolean returns.
    Added a for statement and redefined the mailboxes variable.

--- RunAudit.ps1
+++ RunAudit.ps1
@@ -1,5 +1,6 @@
 $UPN = Read-Host "Enter the UPN"
 Connect-ExchangeOnline -UserPrincipalName $UPN
+
 $orgAudit = (Get-OrganizationConfig).AuditDisabled
 if (!$orgAudit) {
     Write-Host "Audit is enabled for the organization, setting audit status for mailboxes..."
@@ -11,12 +12,19 @@ else {
     Get-Mailbox | Set-Mailbox -AuditEnabled $true
 }
 
-$mailAudit = (Get-Mailbox).AuditEnabled
-if (!$mailAudit) {
-    Write-Host "Audit isn't enabled for all mailboxes, enabling..."
-    Get-Mailbox | Set-Mailbox -AuditEnabled $true
+$mailboxes = Get-Mailbox
+$auditNotEnabled = $false
+foreach ($mailbox in $mailboxes) {
+    if (-not $mailbox.AuditEnabled) {
+        $auditNotEnabled = $true
+        break
+    }
 }
-else {
+
+if ($auditNotEnabled) {
+    Write-Host "Audit isn't enabled for all mailboxes, enabling..."
+    $mailboxes | Set-Mailbox -AuditEnabled $true
+} else {
     Write-Host "Audit is enabled for all mailboxes, exiting..."
     Disconnect-ExchangeOnline -Confirm:$false
 }
